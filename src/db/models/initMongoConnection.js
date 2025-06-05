ffdc2d30ad83a4a65846b9a1f0b14b5ef1312faa
Maryna <tromsa3122@mail.com>
2025-06-05 16:36:16 +0300

    fix

--- src/db/models/initMongoConnection.js
+++ src/db/models/initMongoConnection.js
@@ -10,8 +10,7 @@ export const initMongoConnection = async () => {
     const db = getEnvVar('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0
-`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
     );
     console.log('Mongo connection successfully established!');
   } catch (e) {
