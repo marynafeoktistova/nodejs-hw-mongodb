c0f805bd85347bd5896b18b9c9a9cc79ba2921ad
Maryna <tromsa3122@mail.com>
2025-07-13 14:50:52 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -1,24 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import cookieParser from 'cookie-parser';
-import swaggerUi from 'swagger-ui-express';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 import { getEnvVar } from './utils/getEnvVar.js';
 import contactsRouter from './routers/contacts.js';
-import authRouter from './routers/auth.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const swaggerDocument = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../docs/swagger.json'), 'utf-8'),
-);
+import authRouter from './routers/auth.js';
+import cookieParser from 'cookie-parser';
+import { swaggerDocs } from '../src/middlewares/swaggerDocs.js';
 
 export const startServer = () => {
   const app = express();
@@ -27,20 +16,21 @@ export const startServer = () => {
   app.use(express.json());
   app.use(cors());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
+
   app.use(cookieParser());
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.use('/auth', authRouter);
+
   app.use('/contacts', contactsRouter);
 
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
   app.use(notFoundHandler);
+
   app.use(errorHandler);
 
   app.listen(PORT, () => {
-    console.log(`🚀 Server is running on port ${PORT}`);
-    console.log(
-      `📄 Swagger docs available at http://localhost:${PORT}/api-docs`,
-    );
+    console.log(`Server is running on port ${PORT}`);
   });
 };
